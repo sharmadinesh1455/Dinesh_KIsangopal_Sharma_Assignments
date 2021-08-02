49862439008cfca4fe159d94d27da12cb83b97e1
sharmadinesh1455 <86960291+sharmadinesh1455@users.noreply.github.com>
2021-08-02 16:28:23 +0530

    Add files via upload

---
+++ Spring mvc/Q1/Q1/HelloworldController.java
@@ -0,0 +1,25 @@
+/*package com.mvc.assignment;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/index.do")
+public class HelloworldController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException
+	{
+		request.getRequestDispatcher("/views/index.jsp").forward(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException
+	{
+		request.getRequestDispatcher("/views/welcome.jsp").forward(request, response);
+	}
+}*/
